a666bd774acbe2e76f87d14384fb2493a6e21921
Himanshu Singh <110761890+HimanshuSingh82@users.noreply.github.com>
2025-04-10 17:08:10 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,7 +5,7 @@ import profile_image from './him.jpg'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const url = 'http://localhost:4000'
+export const url = 'https://foodie-backend-w8ov.onrender.com'
 export const currency = '₹'
 
 export const assets ={
